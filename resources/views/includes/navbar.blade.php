0f8cda1c8c47db5a525058cc43402b0cf8d6129b
Mammuasyir <humammuasyir@gmail.com>
2022-04-14 01:05:13 +0700

    navbar-fix

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,17 +1,17 @@
 <header>
-        <nav class="navbar navbar-expand-lg navbar-light fixed-top bg-light ">
+<nav class="navbar navbar-expand-lg navbar-light fixed-top bg-light">
             <div class="container">
-                <a href="/" aria-current="page"> <img class="img-fluid" src="{{asset('landing/img/logo.png')}}" style="width: 170px;"></a>
+                <a href="/" aria-current="page"> <img class="img-fluid" src="{{asset('landing/img/logo.png')}}"></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-5">
                         <li class="nav-item">
-                            <a class="nav-link active" style="color: #33334F; font-family: Open Sans;" aria-current="page" href="#">HOME</a>
+                            <a class="nav-link active" aria-current="page" href="#">HOME</a>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" style="color: #33334F; font-family: Open Sans;" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 PRODUCT
                             </a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -25,10 +25,10 @@
 
                     <div class="d-flex">
                         @guest
-                        <a class="nav-link active" style="color: #33334F;" aria-current="page" href="#"><i class="fi fi-rr-search"></i></a>
-                        <a class="nav-link active" style="color: #33334F;" aria-current="page" href="#"><i class="fi fi-rr-shopping-cart"></i></a>
-                        <a href="#" class="btn btn-outline-secondary me-2" type="submit">Sign In</a>
-                        <a href="#" class="btn" style="color: #FFC806; background-color:#000F48;" type="submit">Sign Up</a>
+                        <a class="nav-link active" aria-current="page" href="#"><i class="fi fi-rr-search"></i></a>
+                        <a class="nav-link active" aria-current="page" href="#"><i class="fi fi-rr-shopping-cart"></i></a>
+                        <a href="#" class="btn btn-login me-2" type="submit">Sign In</a>
+                        <a href="#" class="btn daftar" type="submit">Sign Up</a>
                         @else
                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                             <a class="nav-link active anchor" aria-current="page" href="#"><i class="fas fa-search"></i> Cari</a>
